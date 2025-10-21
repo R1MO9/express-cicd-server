e42e36665c498e0796a64abc18b818b8d68ce7e1
R1MO9 <rupankardas53@gmail.com>
2025-10-22 00:22:37 +0530

    fix: update default server port from 3000 to 5000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import morgan from 'morgan';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(helmet());
